4c6821e5e97c2de9f9d2ca60382656326d87fcb6
Lawal Aminu Saleh <lawie.aminu@yahoo.com>
2024-02-01 14:20:24 +0100

    app page created with boilerplate

---
+++ app.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import { Application } from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app: Application = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const PORT: number = 8000
+
+app.listen(PORT, async () => {
+    console.log(`Server connected on port ${PORT}`)
+    try {
+        await mongoose.connect(
+            process.env.DATABASE_URL as string
+        )
+        console.log("Connected to Database")
+    } catch (error) {
+        console.log('Error connecting to database')
+    }
+})
